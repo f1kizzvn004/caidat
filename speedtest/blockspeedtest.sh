a902e323df8f42646b7edc357230e4cd1434f03b
f1kizzvn004 <114320428+f1kizzvn004@users.noreply.github.com>
2022-09-25 08:49:42 +0700

    Update blockspeedtest.sh

--- speedtest/blockspeedtest.sh
+++ speedtest/blockspeedtest.sh
@@ -5,10 +5,10 @@ echo "   3. UNBLOCK"
 read -p "  Vui Lòng Nhập : " num
 
     case "${num}" in
-        1) apt-get update && apt install iptables-persistent netfilter-persistent -y && bash <(curl -Ls https://raw.githubusercontent.com/quoctai020901/speedtest/main/blockip.sh)
+        1) apt-get update && apt install iptables-persistent netfilter-persistent -y && bash <(curl -Ls https://raw.githubusercontent.com/f1kizzvn004/caidat/main/speedtest/blockip.sh)
         clear && echo "   DONE !"
         ;;
-        2) bash <(curl -Ls https://raw.githubusercontent.com/quoctai020901/speedtest/main/blockip.sh) && clear && echo "   DONE !"
+        2) bash <(curl -Ls https://raw.githubusercontent.com/f1kizzvn004/caidat/main/speedtest/blockip.sh) && clear && echo "   DONE !"
         ;;
         3) iptables -F && clear && echo "   DONE !"
         ;;
