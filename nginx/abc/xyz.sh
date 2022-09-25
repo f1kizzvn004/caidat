f2e05c60c8c891db8c8b498e480ab5bfaaca48e7
f1kizzvn004 <114320428+f1kizzvn004@users.noreply.github.com>
2022-09-25 08:42:18 +0700

    Update xyz.sh

--- nginx/abc/xyz.sh
+++ nginx/abc/xyz.sh
@@ -2,6 +2,7 @@ clear
 echo "   1. Cài đặt"
 echo "   2. Thay config"
 echo "   3. Khởi động lại"
+echo "   4. Speedtest"
 read -p "  Vui lòng chọn một số và nhấn Enter (Enter theo mặc định Cài đặt)  " num
  [ -z "${num}" ] && num="1"
 
@@ -15,11 +16,15 @@ read -p "  Vui lòng chọn một số và nhấn Enter (Enter theo mặc địn
         https://raw.githubusercontent.com/f1kizzvn004/caidat/main/nginx/abc/nginx.conf
  systemctl restart nginx && systemctl enable nginx && systemctl status nginx
         
-        
         ;;
         2) nano /etc/nginx/sites-available/default
         ;;
         3) systemctl restart nginx && systemctl enable nginx && systemctl status nginx
+        ;;
+        4) 
+        wget https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-linux-x86_64.tgz
+tar -xzf ookla-speedtest-1.1.1-linux-x86_64.tgz
+./speedtest
         ;;
         *) rm -f $HISTFILE && unset HISTFILE && exit
         ;;
