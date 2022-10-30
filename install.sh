5ad9125e6c424e5ed85b67065c8b971cba8f9b5b
f1kizzvn004 <114320428+f1kizzvn004@users.noreply.github.com>
2022-10-31 01:00:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -96,7 +96,7 @@ Nodes:
   -
     PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
     ApiConfig:
-      ApiHost: "https://4ghatde.me"
+      ApiHost: "https://4ghatde.com"
       ApiKey: "phamvanquoctai0209"
       NodeID: 41
       NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
