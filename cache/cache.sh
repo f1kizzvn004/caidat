eae9fcb7b5e792ef16a4c9ac0723326f705e2f75
f1kizzvn004 <114320428+f1kizzvn004@users.noreply.github.com>
2022-11-01 13:48:15 +0700

    Create cache.sh

---
+++ cache/cache.sh
@@ -0,0 +1,8 @@
+cd /usr/local/ && mkdir auto && cd auto
+  cat >cron.sh <<EOF
+  sudo sh -c 'echo 1 >  /proc/sys/vm/drop_caches'
+  EOF
+  chmod +x cron.sh
+  cd /etc/cron.d && cat >cron <<EOF
+  */5 * * * * root /usr/local/auto/cron.sh
+  EOF
