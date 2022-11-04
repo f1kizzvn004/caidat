a0157b0d1d0ae1cb5616e31a80cc4619748948fc
f1kizzvn004 <114320428+f1kizzvn004@users.noreply.github.com>
2022-11-04 19:30:23 +0700

    Update cache.sh

--- cache/cache.sh
+++ cache/cache.sh
@@ -2,9 +2,9 @@ cd /usr/local/ && rm -rf auto && mkdir auto && cd auto
 cat >cron.sh <<EOF
 sudo sh -c 'echo 1 >  /proc/sys/vm/drop_caches'
 EOF
-chmod +x cron.sh
+chmod 755 cron.sh
 cd /etc/cron.d
 cat >cron <<EOF
-* * * * * root /usr/local/auto/cron.sh
+* * * * *  /usr/local/auto/cron.sh
 EOF
 cd /root
