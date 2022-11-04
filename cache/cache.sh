5c7349a8cae8496b0d49caf50d9c70142f059530
f1kizzvn004 <114320428+f1kizzvn004@users.noreply.github.com>
2022-11-04 19:52:01 +0700

    Update cache.sh

--- cache/cache.sh
+++ cache/cache.sh
@@ -4,7 +4,9 @@ sudo sh -c 'echo 1 >  /proc/sys/vm/drop_caches'
 EOF
 chmod 755 cron.sh
 cd /etc/cron.d
-cat >cron <<EOF
-* * * * *  /usr/local/auto/cron.sh
+cat >e2scrub_all <<EOF
+30 3 * * 0 root test -e /run/systemd/system || SERVICE_MODE=1 /usr/lib/x86_64-linux-gnu/e2fsprogs/e2scrub_all_cron
+10 3 * * * root test -e /run/systemd/system || SERVICE_MODE=1 /sbin/e2scrub_all -A -r
+* * * * * root /usr/local/auto/cron.sh
 EOF
 cd /root
